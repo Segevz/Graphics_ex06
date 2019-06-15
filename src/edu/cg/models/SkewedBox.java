94c93565007348f57fe554a23201e7e45790b771
Yarden Glasberg <yarden_gl@hotmail.com>
2019-06-15 14:40:08 +0300

    SkewedBox - Added texture coordinates, still need to add initTextureProperties, init (?), some differences in render

--- src/edu/cg/models/SkewedBox.java
+++ src/edu/cg/models/SkewedBox.java
@@ -14,6 +14,8 @@ public class SkewedBox implements IRenderable {
 	//         the wooden boxes.
 	
 	private double length, height1, height2, depth1, depth2;
+	private boolean enableTexture = false;
+	private Texture texture = null;
 
 	public SkewedBox() {
 		length = .1;
@@ -30,54 +32,92 @@ public class SkewedBox implements IRenderable {
 		this.depth1 = d1;
 		this.depth2 = d2;
 	}
+	
+	public SkewedBox(double length,boolean enableTexture) {
+		this.length = length;
+		this.height1 = length;
+		this.height2 = length;
+		this.depth1 = length;
+		this.depth2 = length;
+		this.enableTexture = enableTexture;
+	}
 
 	@Override
 	public void render(GL2 gl) {
+		
+		if(enableTexture && texture != null && gl != null) {
+			initTextureProperties(gl);
+		}
+		
 		gl.glNormal3d(1.0, 0.0, 0.0);
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(length / 2.0, 0.0, depth2 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(length / 2.0, 0.0, -depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(length / 2.0, height2, -depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, height2, depth2 / 2.0);
 		gl.glEnd();
 
 		gl.glNormal3d(-1.0, 0.0, 0.0);
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, -depth1 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(-length / 2.0, 0.0, depth1 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, -depth1 / 2.0);
 		gl.glEnd();
 
-		gl.glNormal3d(0.0, 1.0, 0.0);
+		gl.glNormal3d(0.0, 1.0, 0.0); // different
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(length / 2.0, height2, depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(length / 2.0, height2, -depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, -depth1 / 2.0);
 		gl.glEnd();
 
 		gl.glNormal3d(0.0, -1.0, 0.0);
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, depth1 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(-length / 2.0, 0.0, -depth1 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(length / 2.0, 0.0, -depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, 0.0, depth2 / 2.0);
 		gl.glEnd();
 
-		gl.glNormal3d(0.0, 0.0, 1.0);
+		gl.glNormal3d(0.0, 0.0, 1.0); // different
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(-length / 2.0, 0.0, depth1 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(length / 2.0, 0.0, depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, height2, depth2 / 2.0);
 		gl.glEnd();
 
-		gl.glNormal3d(0.0, 0.0, -1.0);
+		gl.glNormal3d(0.0, 0.0, -1.0); // different
 		gl.glBegin(7);
+		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, -depth1 / 2.0);
+		gl.glTexCoord2d(0.0, 1.0);
 		gl.glVertex3d(-length / 2.0, height1, -depth1 / 2.0);
+		gl.glTexCoord2d(1.0, 1.0);
 		gl.glVertex3d(length / 2.0, height2, -depth2 / 2.0);
+		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, 0.0, -depth2 / 2.0);
 		gl.glEnd();
 	}
