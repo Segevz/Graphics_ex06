058af24176dd1817268fa150cccf222fb170a522
Yarden Glasberg <yarden_gl@hotmail.com>
2019-06-15 16:02:24 +0300

    F1Car - Done

--- src/edu/cg/models/Car/F1Car.java
+++ src/edu/cg/models/Car/F1Car.java
@@ -18,6 +18,9 @@ public class F1Car implements IRenderable {
 	//       * You also need to setup material properties for different
 	//       * components of the car.
 	
+	Center center;
+	Back back;
+	Front front;
 
 	@Override
 	public void render(GL2 gl) {
@@ -25,17 +28,15 @@ public class F1Car implements IRenderable {
 		// Here You should only render the three parts: back, center and front.
 		
 		// Render center of car
-		Center center = new Center();
+	
 		gl.glPushMatrix();
 		center.render(gl);
 
 		// Render back of car
-		Back back = new Back();
 		gl.glTranslated((-1) * (Specification.B_LENGTH + Specification.C_BASE_LENGTH) / 2, 0, 0);
 		back.render(gl);
 		
 		// Render front of car
-		Front front = new Front();
 		gl.glTranslated((Specification.F_FRONT_LENGTH + Specification.B_LENGTH) / 2 + Specification.C_BASE_LENGTH, 0, 0);
 		front.render(gl);
 
@@ -49,6 +50,13 @@ public class F1Car implements IRenderable {
 
 	@Override
 	public void init(GL2 gl) {
+		center = new Center();
+		center.init(gl);
 
+		back = new Back();
+		back.init(gl);
+
+		front = new Front();
+		front.init(gl);
 	}
 }
