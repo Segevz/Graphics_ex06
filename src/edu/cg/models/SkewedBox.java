e66b7308a74eae90f24a5b64579e6a6c4fbfa6d9
Yarden Glasberg <yarden_gl@hotmail.com>
2019-06-15 15:19:55 +0300

    SkewedBox - Done

--- src/edu/cg/models/SkewedBox.java
+++ src/edu/cg/models/SkewedBox.java
@@ -15,14 +15,14 @@ public class SkewedBox implements IRenderable {
 	
 	private double length, height1, height2, depth1, depth2;
 	private boolean enableTexture = false;
-	private Texture texture = null;
+	private Texture textureBox = null;
 
 	public SkewedBox() {
-		length = .1;
-		height1 = .2;
-		height2 = .1;
-		depth1 = .2;
-		depth2 = .1;
+		length = .8;
+		height1 = .7;
+		height2 = .5;
+		depth1 = .7;
+		depth2 = .3;
 	};
 
 	public SkewedBox(double length, double h1, double h2, double d1, double d2) {
@@ -45,9 +45,11 @@ public class SkewedBox implements IRenderable {
 	@Override
 	public void render(GL2 gl) {
 		
-		if(enableTexture && texture != null && gl != null) {
-			initTextureProperties(gl);
-		}
+		Vec normal = null;
+	       if (this.useTexture) {
+	           assert (texureBox != null && gl != null);
+	           this.initTexture(gl);
+	       }
 		
 		gl.glNormal3d(1.0, 0.0, 0.0);
 		gl.glBegin(7);
@@ -59,10 +61,8 @@ public class SkewedBox implements IRenderable {
 		gl.glVertex3d(length / 2.0, height2, -depth2 / 2.0);
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, height2, depth2 / 2.0);
-		gl.glEnd();
 
 		gl.glNormal3d(-1.0, 0.0, 0.0);
-		gl.glBegin(7);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, -depth1 / 2.0);
 		gl.glTexCoord2d(0.0, 1.0);
@@ -71,10 +71,9 @@ public class SkewedBox implements IRenderable {
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, -depth1 / 2.0);
-		gl.glEnd();
 
-		gl.glNormal3d(0.0, 1.0, 0.0); // different
-		gl.glBegin(7);
+		normal = new Vec(height1 - height2, 1.0, 0.0).normalize();
+        gl.glNormal3d(normal.x, normal.y, normal.z);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
 		gl.glTexCoord2d(0.0, 1.0);
@@ -83,10 +82,8 @@ public class SkewedBox implements IRenderable {
 		gl.glVertex3d(length / 2.0, height2, -depth2 / 2.0);
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, -depth1 / 2.0);
-		gl.glEnd();
 
 		gl.glNormal3d(0.0, -1.0, 0.0);
-		gl.glBegin(7);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, depth1 / 2.0);
 		gl.glTexCoord2d(0.0, 1.0);
@@ -95,10 +92,9 @@ public class SkewedBox implements IRenderable {
 		gl.glVertex3d(length / 2.0, 0.0, -depth2 / 2.0);
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, 0.0, depth2 / 2.0);
-		gl.glEnd();
 
-		gl.glNormal3d(0.0, 0.0, 1.0); // different
-		gl.glBegin(7);
+		normal = new Vec(depth1 - depth2, 0.0, 1.0).normalize();
+        gl.glNormal3d(normal.x, 0.0, normal.z);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, height1, depth1 / 2.0);
 		gl.glTexCoord2d(0.0, 1.0);
@@ -107,10 +103,9 @@ public class SkewedBox implements IRenderable {
 		gl.glVertex3d(length / 2.0, 0.0, depth2 / 2.0);
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, height2, depth2 / 2.0);
-		gl.glEnd();
 
-		gl.glNormal3d(0.0, 0.0, -1.0); // different
-		gl.glBegin(7);
+		normal = new Vec(this.depth1 - this.depth2, 0.0, -1.0).normalize();
+        gl.glNormal3d(normal.x, 0.0, normal.z);
 		gl.glTexCoord2d(0.0, 0.0);
 		gl.glVertex3d(-length / 2.0, 0.0, -depth1 / 2.0);
 		gl.glTexCoord2d(0.0, 1.0);
@@ -120,11 +115,37 @@ public class SkewedBox implements IRenderable {
 		gl.glTexCoord2d(1.0, 0.0);
 		gl.glVertex3d(length / 2.0, 0.0, -depth2 / 2.0);
 		gl.glEnd();
+		
+		gl.glDisable(GL2.GL_TEXTURE_2D);
 	}
+	
+	private void initTexture(GL2 gl) {
+        gl.glEnable(GL2.GL_TEXTURE_2D);
+        this.textureBox.bind(gl);
+        gl.glTexEnvi(GL2.GL_TEXTURE_ENV, GL2.GL_TEXTURE_ENV_MODE, GL2.GL_MODULATE);
+        gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MIN_FILTER, GL2.GL_LINEAR_MIPMAP_LINEAR);
+        gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MAG_FILTER, GL2.GL_LINEAR);
+        gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_MAX_LOD, 1);
+    }
 
 	@Override
 	public void init(GL2 gl) {
+        if (enableTexture) {
+            try {
+                textureBox = TextureIO.newTexture(new File("Textures/WoodBoxTexture.jpg"), true);
+            }
+            catch (Exception e) {
+                System.err.print("Unable to read texture : " + e.getMessage());
+            } 
+        }
 	}
+	
+    public void destroy(GL2 gl) {
+        if (this.enableTexture) {
+            this.textureBox.destroy(gl);
+            this.textureBox = null;
+        }
+    }
 	
 	@Override
 	public String toString() {
