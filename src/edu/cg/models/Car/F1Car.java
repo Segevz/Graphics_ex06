80b29dc38ee34343bf75659afe8e739227faa532
Yarden Glasberg <yarden_gl@hotmail.com>
2019-06-15 11:34:27 +0300

    Copied F1Car implementation from ex5, still need to copy component src files - Back.java etc

--- src/edu/cg/models/Car/F1Car.java
+++ src/edu/cg/models/Car/F1Car.java
@@ -17,4 +17,38 @@ public class F1Car implements IRenderable {
 	// 		 * We deleted all components source files (Back.java, Center.java...), so put your components implementation as well.
 	//       * You also need to setup material properties for different
 	//       * components of the car.
+	
+
+	@Override
+	public void render(GL2 gl) {
+		// Render the whole car. 
+		// Here You should only render the three parts: back, center and front.
+		
+		// Render center of car
+		Center center = new Center();
+		gl.glPushMatrix();
+		center.render(gl);
+
+		// Render back of car
+		Back back = new Back();
+		gl.glTranslated((-1) * (Specification.B_LENGTH + Specification.C_BASE_LENGTH) / 2, 0, 0);
+		back.render(gl);
+		
+		// Render front of car
+		Front front = new Front();
+		gl.glTranslated((Specification.F_FRONT_LENGTH + Specification.B_LENGTH) / 2 + Specification.C_BASE_LENGTH, 0, 0);
+		front.render(gl);
+
+		gl.glPopMatrix();
+	}
+
+	@Override
+	public String toString() {
+		return "F1Car";
+	}
+
+	@Override
+	public void init(GL2 gl) {
+
+	}
 }
